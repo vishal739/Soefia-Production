386e9b813a7fa57e1fa2f0eb27b28ecbf4098cfa
Vishal Kumar Singh <136896825+vishal739@users.noreply.github.com>
2024-08-18 13:39:01 +0530

    Update auth.js

--- Backend/auth.js
+++ Backend/auth.js
@@ -25,7 +25,7 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, passwor
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:8080/auth/google/callback",
+  callbackURL: "https://soefia-production.onrender.com/auth/google/callback",
   passReqToCallback: true
 },
   async (request, accessToken, refreshToken, profile, done) => {
