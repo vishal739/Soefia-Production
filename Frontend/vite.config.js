bb3e621ced4211c0c76ebed89bcc9a7b33e5cbbb
vishal739 <vishalsinghkr739@gmail.com>
2024-09-03 14:48:55 +0530

    AUTH TESTING 3.0

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy: {
-      '/api': "https://soefia-production.onrender.com"
+      '/api': {
+        target: 'https://soefia-production.onrender.com/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   }
 })
