d270e08ae4c70c37900b4d08411c49089edbe0aa
vishal739 <vishalsinghkr739@gmail.com>
2024-09-03 14:33:07 +0530

    auth testing

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy: {
-      '/api': import.meta.env.VITE_REACT_APP_SERVER_URL
+      '/api': `${import.meta.env.VITE_REACT_APP_SERVER_URL}`
     },
   }
 })
