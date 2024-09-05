56e55d417aed2504dc3f431fcf2ebaa9b3e6a441
vishal739 <vishalsinghkr739@gmail.com>
2024-09-05 13:06:42 +0530

    Deploy Vercel testing

--- Backend/model/schoolModel.js
+++ Backend/model/schoolModel.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schoolSchema = new Schema({
-  name: { type: String, required: true },
-  address: { type: String, required: true },
-  contactNumber: { type: String, required: true },
-  teachers: [{ type: Schema.Types.ObjectId, ref: 'Teacher' }],
-  students: [{ type: Schema.Types.ObjectId, ref: 'Student' }],
-  admins: [{ type: Schema.Types.ObjectId, ref: 'Admin' }]
-},{ timestamps: true });
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    contactNumber: { type: String, required: true },
+    teachers: [{ type: Schema.Types.ObjectId, ref: 'Teacher' }],
+    students: [{ type: Schema.Types.ObjectId, ref: 'Student' }],
+    admins: [{ type: Schema.Types.ObjectId, ref: 'Admin' }]
+}, { timestamps: true });
 
 const School = mongoose.model('School', schoolSchema);
+module.exports = School;
--- Backend/model/studentModel.js
+++ Backend/model/studentModel.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  profileBio: { type: String },
-  class: { type: String, required: true },
-  academicFactors: [{
-    subjectName: { type: String },
-    grade: { type: String },
-    performance: { type: String },
-  }],
-  school: { type: Schema.Types.ObjectId, ref: 'School' }
-},{ timestamps: true });
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    profileBio: { type: String },
+    class: { type: Schema.Types.ObjectId, ref: 'Class', required: true }, 
+    academicFactors: [{
+        subjectName: { type: String },
+        grade: { type: String },
+        performance: { type: String },
+    }],
+    school: { type: Schema.Types.ObjectId, ref: 'School' }
+}, { timestamps: true });
 
 const Student = mongoose.model('Student', studentSchema);
 module.exports = Student;
